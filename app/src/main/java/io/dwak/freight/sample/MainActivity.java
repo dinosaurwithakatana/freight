24574c7d063ba9fa8d1c0a9eb5e1dafafc2924be
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2016-04-18 01:24:01 -0500

    include activity

---
+++ app/src/main/java/io/dwak/freight/sample/MainActivity.java
@@ -0,0 +1,28 @@
+package io.dwak.freight.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+
+import com.bluelinelabs.conductor.Conductor;
+import com.bluelinelabs.conductor.Router;
+
+public class MainActivity extends AppCompatActivity {
+
+  private Router router;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ViewGroup container = (ViewGroup) findViewById(R.id.container);
+
+    router = Conductor.attachRouter(this, container, savedInstanceState);
+    if(!router.hasRootController()){
+      router.setRoot(new MainControllerBuilder()
+              .testBooleanExtra(false)
+              .testStringExtra("Yeah this is a string")
+              .testIntExtra(1)
+              .build());
+    }
+  }
+}
