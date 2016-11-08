e2c31ca070f0b1b6f804c8e4e94b6b3516952a0d
Vishnu Rajeevan <vishnu@Vishnus-MacBook-Pro.local>
2016-11-08 01:15:09 -0500

    fix builder packages when generating navigators

--- freight-processor/src/main/kotlin/io/dwak/freight/processor/model/ClassBinding.kt
+++ freight-processor/src/main/kotlin/io/dwak/freight/processor/model/ClassBinding.kt
@@ -3,8 +3,10 @@ package io.dwak.freight.processor.model
 import io.dwak.freight.annotation.ControllerBuilder
 import io.dwak.freight.annotation.Extra
 import io.dwak.freight.processor.extension.hasAnnotationWithName
+import io.dwak.freight.processor.extension.packageName
 import javax.lang.model.element.Element
 import javax.lang.model.element.ElementKind
+import javax.lang.model.element.TypeElement
 import javax.lang.model.type.TypeMirror
 
 class ClassBinding(element : Element) : Binding {
